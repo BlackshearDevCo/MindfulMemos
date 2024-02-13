2158517415275254c8d589797b9e0c061817eb9c
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-02-12 22:31:04 -0700

    Add SpeedInsights component to track site performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Link from "next/link";
 import { Button } from "@/components/ui/Button";
@@ -35,6 +36,8 @@ export default function RootLayout({
           {children}
         </div>
       </body>
+
+      <SpeedInsights />
     </html>
   );
 }

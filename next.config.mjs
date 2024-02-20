5349e1bda2387dae6a6e2ffc43ba87daaec4d3cf
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-02-20 16:28:50 -0700

    Update vercel env variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL ?? process.env.VERCEL_URL,
+    AUTH0_BASE_URL:
+      process.env.AUTH0_BASE_URL ?? `https://${process.env.VERCEL_URL}`,
   },
   async redirects() {
     return [

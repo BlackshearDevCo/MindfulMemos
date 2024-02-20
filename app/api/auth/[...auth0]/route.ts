8c8abcd901ffeb9c3832d1ef01fbb98be63a06ae
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-02-20 15:38:50 -0700

    Fix auth0

--- app/api/auth/[...auth0]/route.ts
+++ app/api/auth/[...auth0]/route.ts
@@ -1,10 +1,3 @@
 import auth0 from "@/lib/auth0";
-import { handleLogin } from "@auth0/nextjs-auth0";
 
-export const GET = auth0.handleAuth({
-  login: handleLogin({
-    authorizationParams: {
-      audience: process.env.AUTH0_AUDIENCE,
-    },
-  }),
-});
+export const GET = auth0.handleAuth();

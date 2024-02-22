37a8891e3179cac8d16cd9482862a500e9fc93a2
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-02-22 14:37:23 -0700

    Refactored user image

--- components/ui/UserImage.tsx
+++ components/ui/UserImage.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { useUser } from "@/lib/hooks";
+import { getUsersInitials } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
 export default function UserImage() {
   const user = useUser();
+  const userInitials = getUsersInitials(user);
 
   return (
     <div className="flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-background-100">
       {user?.image ? (
         <Image src={user.image} alt="Profile Image" width={64} height={64} />
       ) : (
-        <span className="text-2xl font-bold">BB</span>
+        <span className="text-2xl font-bold">{userInitials}</span>
       )}
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,17 @@ export function cn(...inputs: ClassValue[]) {
 export function getUsersName(user?: User) {
   return user?.name ?? user?.email ?? "User";
 }
+
+export function getUsersInitials(user?: User) {
+  const name = user?.name;
+  if (!user || !name) return "MM";
+
+  const splitName = name.split(" ");
+
+  if (splitName.length === 1) return splitName.splice(0, 2);
+
+  return splitName
+    .map((nameSegment) => nameSegment.charAt(0))
+    .join("")
+    .slice(0, 2);
+}

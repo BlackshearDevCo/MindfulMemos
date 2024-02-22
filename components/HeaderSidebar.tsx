9b2e2b22d2e673cb14ed9e9e2b65415020606f7e
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-02-21 23:20:05 -0700

    Added transition for sidebar

--- components/HeaderSidebar.tsx
+++ components/HeaderSidebar.tsx
@@ -16,6 +16,7 @@ import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { getUsersFullName } from "@/lib/utils";
 import { getLogoutRoute } from "@/lib/routes";
+import { Transition } from "@headlessui/react";
 
 export default function HeaderSidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -40,79 +41,104 @@ function Sidebar({
 }) {
   const { user } = useUser();
 
-  // TODO: Make accessible
+  const sidebarClasses =
+    "absolute left-0 top-0 z-50 flex h-screen w-[80vw] flex-col items-center rounded-r-xl bg-background-50 pb-16 pt-8 shadow-lg ring-1 ring-gray-900/5";
+
   return (
-    <div className={isOpen ? "visible block" : "invisible hidden"}>
-      <div className="absolute left-0 top-0 z-50 flex h-screen w-[80vw] flex-col items-center rounded-r-xl bg-background-50 pb-16 pt-8 shadow-lg ring-1 ring-gray-900/5">
-        <button
-          onClick={closeSidebar}
-          className="absolute right-4 top-4 h-6 w-6 rounded-md focus:bg-text-200"
-          tabIndex={0}
+    <Transition show={isOpen}>
+      <div>
+        <Transition.Child
+          className={sidebarClasses}
+          enter="transition ease-in-out duration-300"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
         >
-          <XMarkIcon />
-        </button>
+          <div className={sidebarClasses}>
+            <button
+              onClick={closeSidebar}
+              className="absolute right-4 top-4 h-6 w-6 rounded-md focus:bg-text-200"
+              tabIndex={0}
+            >
+              <XMarkIcon />
+            </button>
 
-        <section className="mb-3 flex flex-col items-center">
-          <h2 className="mb-3 text-center text-base font-bold">mindfulmemos</h2>
-          <div className="mb-4">
-            <UserImage />
+            <section className="mb-3 flex flex-col items-center">
+              <h2 className="mb-3 text-center text-base font-bold">
+                mindfulmemos
+              </h2>
+              <div className="mb-4">
+                <UserImage />
+              </div>
+              <h2 className="mb-1 text-center text-2xl font-bold">
+                {getUsersFullName(user)}
+              </h2>
+              <Link href="/" className="text-sm text-accent-500">
+                View account
+              </Link>
+            </section>
+
+            <nav className="flex w-full flex-1 flex-col justify-between">
+              <div className="flex flex-col items-start">
+                <NavItem href="/">
+                  <IconWrapper>
+                    <MagnifyingGlassIcon />
+                  </IconWrapper>
+                  <span>Search</span>
+                </NavItem>
+                <NavItem href="/">
+                  <IconWrapper>
+                    <ClipboardIcon />
+                  </IconWrapper>
+                  <span>Tasks</span>
+                </NavItem>
+                <NavItem href="/">
+                  <IconWrapper>
+                    <CloudIcon />
+                  </IconWrapper>
+                  <span>Thoughts</span>
+                </NavItem>
+                <NavItem href="/">
+                  <IconWrapper>
+                    <ListBulletIcon />
+                  </IconWrapper>
+                  <span>Categories</span>
+                </NavItem>
+                <NavItem asButton>
+                  <IconWrapper>
+                    <MoonIcon />
+                  </IconWrapper>
+                  <span>Theme</span>
+                </NavItem>
+              </div>
+
+              <NavItem href={getLogoutRoute()}>
+                <IconWrapper>
+                  <ArrowLeftStartOnRectangleIcon />
+                </IconWrapper>
+                <span>Sign out</span>
+              </NavItem>
+            </nav>
           </div>
-          <h2 className="mb-1 text-center text-2xl font-bold">
-            {getUsersFullName(user)}
-          </h2>
-          <Link href="/" className="text-sm text-accent-500">
-            View account
-          </Link>
-        </section>
+        </Transition.Child>
 
-        <nav className="flex w-full flex-1 flex-col justify-between">
-          <div className="flex flex-col items-start">
-            <NavItem href="/">
-              <IconWrapper>
-                <MagnifyingGlassIcon />
-              </IconWrapper>
-              <span>Search</span>
-            </NavItem>
-            <NavItem href="/">
-              <IconWrapper>
-                <ClipboardIcon />
-              </IconWrapper>
-              <span>Tasks</span>
-            </NavItem>
-            <NavItem href="/">
-              <IconWrapper>
-                <CloudIcon />
-              </IconWrapper>
-              <span>Thoughts</span>
-            </NavItem>
-            <NavItem href="/">
-              <IconWrapper>
-                <ListBulletIcon />
-              </IconWrapper>
-              <span>Categories</span>
-            </NavItem>
-            <NavItem asButton>
-              <IconWrapper>
-                <MoonIcon />
-              </IconWrapper>
-              <span>Theme</span>
-            </NavItem>
-          </div>
-
-          <NavItem href={getLogoutRoute()}>
-            <IconWrapper>
-              <ArrowLeftStartOnRectangleIcon />
-            </IconWrapper>
-            <span>Sign out</span>
-          </NavItem>
-        </nav>
+        <Transition.Child
+          enter="transition-opacity ease-linear duration-300 fixed left-0 top-0 z-40 h-screen w-screen"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300 fixed left-0 top-0 z-40 h-screen w-screen"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            onClick={closeSidebar}
+            className="bg-background-900/25 fixed left-0 top-0 z-40 h-screen w-screen"
+          />
+        </Transition.Child>
       </div>
-
-      <div
-        onClick={closeSidebar}
-        className="bg-background-900/25 fixed left-0 top-0 z-40 h-screen w-screen"
-      />
-    </div>
+    </Transition>
   );
 }
 
